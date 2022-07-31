03b86ed9d4648b19e55a5461d82de46f99ef07b1
MatValdejao <matvaldejao123@gmail.com>
2022-07-31 17:51:09 -0400

    uncommented questions

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const inquirer = require("inquirer")
 const generateMarkdown = require("./utils/generateMarkdown.js")
 const fs = require("fs")
-const { generate } = require("rxjs")
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = ["What is your GitHub username? "]
-    // , "What is your email address? ", "What is your project's name? ", "Please write a short description of your project ", "What kind of license shoukd your project have? ", "What command should be run to install dependencies? ", "What command should be run to run tests? ", "What does the user need to know about using the repo? ", "What does the user need to know about contributing to the repo? "];
+const questions = ["What is your GitHub username? ", "What is your email address? ", "What is your project's name? ", "Please write a short description of your project ", "What kind of license shoukd your project have? ", "What command should be run to install dependencies? ", "What command should be run to run tests? ", "What does the user need to know about using the repo? ", "What does the user need to know about contributing to the repo? "];
 
 const promptUser = () => {
     return inquirer.prompt([
